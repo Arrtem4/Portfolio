fde3b058ab14e2f7a241d5d4867e5b3d239f5ba2
Artem <yakovenko.art1@gmail.com>
2024-04-09 16:31:14 +0500

    navigation and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import ButtonTheme from "./components/buttons/ButtonTheme";
-import ButtonLanguage from "./components/buttons/ButtonLanguage";
-import Header from "./components/Header";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainLayout from "./components/MainLayout";
+import Main from "./pages/Main";
+import About from "./pages/About";
+import Contacts from "./pages/Contacts";
+import NotFound from "./pages/NotFound";
 export default function App() {
     return (
-        <>
-            <Header />
-            <ButtonTheme />
-            <ButtonLanguage />
-            <div className="first">p</div>
-        </>
+        <BrowserRouter basename="/Portfolio">
+            <section className="app">
+                <Routes>
+                    <Route path="/" element={<MainLayout />}>
+                        <Route index element={<Main />}></Route>
+                        <Route path="about" element={<About />}></Route>
+                        <Route path="contacts" element={<Contacts />}></Route>
+                    </Route>
+                    <Route path="*" element={<NotFound />}></Route>
+                </Routes>
+            </section>
+        </BrowserRouter>
     );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,13 @@
-import { useTranslation } from "react-i18next";
+import { NavLink } from "react-router-dom";
+
+// import { useTranslation } from "react-i18next";
 export default function Header() {
-    const { t } = useTranslation();
-    return <p>{t("header.first")}</p>;
+    // const { t } = useTranslation();
+    return (
+        <section className="header">
+            <NavLink to="/">1</NavLink>
+            <NavLink to="contacts">1</NavLink>
+            <NavLink to="about">1</NavLink>
+        </section>
+    );
 }
