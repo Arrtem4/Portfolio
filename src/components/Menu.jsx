1ac29ef0abacf8cd39fdac1f0d32414d0a259be3
Artem <yakovenko.art1@gmail.com>
2024-04-16 15:44:43 +0500

    language button

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,4 +1,5 @@
 import { NavLink } from "react-router-dom";
+import { IoMenu } from "react-icons/io5";
 import ButtonLanguage from "./buttons/ButtonLanguage";
 import ButtonTheme from "./buttons/ButtonTheme";
 import { useState } from "react";
@@ -12,14 +13,14 @@ export default function Menu({ size }) {
         return (
             <section className="menu-large">
                 <section className="menu-large_wrapper">
-                    <section className="menu-large_buttons">
-                        <ButtonLanguage />
-                        <ButtonTheme />
-                    </section>
-                    <section className="menu-large_links">
+                    {/* <section className="menu-large_links">
                         <NavLink to="/">1</NavLink>
                         <NavLink to="contacts">2</NavLink>
                         <NavLink to="about">3</NavLink>
+                    </section> */}
+                    <section className="menu-large_buttons">
+                        {/* <ButtonTheme /> */}
+                        <ButtonLanguage />
                     </section>
                 </section>
             </section>
@@ -27,14 +28,21 @@ export default function Menu({ size }) {
     } else {
         return (
             <>
-                <button onClick={() => setMenuIsVisible(!menuIsVisible)}>
-                    visible
-                </button>
-                <section
+                <IoMenu
+                    className="menu-small_icon"
                     onClick={() => setMenuIsVisible(!menuIsVisible)}
+                />
+                <section
                     className={`menu-small ${menuIsVisible ? "" : "hidden"}`}
                 >
-                    <section className="menu-small_wrapper">
+                    <section
+                        className="menu-small_wrapper"
+                        onClick={(event) => {
+                            if (event.currentTarget === event.target) {
+                                setMenuIsVisible(false);
+                            }
+                        }}
+                    >
                         <section className="menu-small_buttons">
                             <ButtonLanguage />
                             <ButtonTheme />
--- src/components/buttons/ButtonLanguage.jsx
+++ src/components/buttons/ButtonLanguage.jsx
@@ -1,8 +1,11 @@
+import { AiOutlineCaretDown } from "react-icons/ai";
 import useLanguage from "../../hooks/useLanguage";
 import i18n from "i18next";
+import { useState } from "react";
 
 export default function ButtonLanguage() {
     const [language, setLanguage] = useLanguage("language", "en");
+    const [active, setActive] = useState(false);
     const handleChange = () => {
         if (language === "en") {
             setLanguage("ru");
@@ -12,14 +15,32 @@ export default function ButtonLanguage() {
             i18n.changeLanguage("en");
         }
     };
-    return <button onClick={handleChange}>ButtonLanguage</button>;
+    const autoClose = () => {
+        setActive(!active);
+        setTimeout(() => {
+            setActive(false);
+        }, 3000);
+    };
+    return (
+        <div className="button-language" onClick={autoClose}>
+            <p className="button-language_value-text">
+                {language === "en" ? "EN" : "RU"}
+            </p>
+            <AiOutlineCaretDown className="button-language_caret" />
+            <ul className={`button-language_list ${active ? "" : "hidden"}`}>
+                <li
+                    className="button-language_list-item"
+                    onClick={handleChange}
+                >
+                    EN
+                </li>
+                <li
+                    className="button-language_list-item"
+                    onClick={handleChange}
+                >
+                    RU
+                </li>
+            </ul>
+        </div>
+    );
 }
-
-// return (
-//     <div className="language-select">
-//         <Select value={language} onChange={handleChange} displayEmpty>
-//             <MenuItem value="en">EN</MenuItem>
-//             <MenuItem value="ru">RU</MenuItem>
-//         </Select>
-//     </div>
-// );
