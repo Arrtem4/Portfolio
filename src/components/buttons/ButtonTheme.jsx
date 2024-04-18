e2b4a82ac6ce06da83d8313ab5098c9deb01e83f
Artem <yakovenko.art1@gmail.com>
2024-04-18 13:10:48 +0500

    button v2 fix

--- src/components/buttons/ButtonTheme.jsx
+++ src/components/buttons/ButtonTheme.jsx
@@ -1,28 +1,61 @@
+import { useState } from "react";
 import useTheme from "../../hooks/useTheme";
 import { BsFillSunFill } from "react-icons/bs";
 import { BsFillMoonStarsFill } from "react-icons/bs";
 
 export default function ButtonTheme() {
     const { theme, setTheme } = useTheme();
-    const handleDivClick2 = () => {
+    const [animationStarted, setAnimationStarted] = useState(false);
+    const handleDivClick = () => {
         if (theme === "light") {
             setTheme("dark");
         } else {
             setTheme("light");
         }
+        setAnimationStarted(true);
     };
 
     return (
-        <div className="button-theme" onClick={handleDivClick2}>
+        <div className="button-theme" onClick={handleDivClick}>
             {theme === "light" && (
-                <div className="button-theme_icon-wrapper">
-                    <BsFillSunFill />
-                </div>
-            )}{" "}
+                <>
+                    <div
+                        className={`${
+                            animationStarted ? "button-theme_icon-wrapper" : ""
+                        }`}
+                    >
+                        <BsFillSunFill />
+                    </div>
+                    <div
+                        className={`${
+                            animationStarted
+                                ? "button-theme_icon-wrapper-hidden"
+                                : ""
+                        } button-theme_icon-wrapper-hidden-static`}
+                    >
+                        <BsFillMoonStarsFill />
+                    </div>
+                </>
+            )}
             {theme === "dark" && (
-                <div className="button-theme_icon-wrapper">
-                    <BsFillMoonStarsFill />
-                </div>
+                <>
+                    <div
+                        className={`${
+                            animationStarted ? "button-theme_icon-wrapper" : ""
+                        }`}
+                    >
+                        <BsFillMoonStarsFill />
+                    </div>
+                    <div
+                        className={`${
+                            animationStarted
+                                ? "button-theme_icon-wrapper-hidden"
+                                : ""
+                        } button-theme_icon-wrapper-hidden-static`}
+                    >
+                        <BsFillSunFill />
+                    </div>
+                </>
             )}
         </div>
     );
