77b14aeb2f91744fa9ba6882c3423a80051cc124
Artem <yakovenko.art1@gmail.com>
2024-04-19 12:18:41 +0500

    not-found

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,3 +1,21 @@
+import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
 export default function NotFound() {
-    return <div>NotFound</div>;
+    const { t } = useTranslation();
+    const navigate = useNavigate();
+
+    return (
+        <section className="not-found center">
+            <p className="not-found_text">{t("not-found.text")}</p>
+            <button className="not-found_button" onClick={() => navigate("/")}>
+                {t("not-found.button-to-home")}
+            </button>
+            <button
+                className="not-found_button"
+                onClick={() => window.history.back()}
+            >
+                {t("not-found.button-back")}
+            </button>
+        </section>
+    );
 }
