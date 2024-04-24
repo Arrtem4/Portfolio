5d0c257e1c031558d0e33ef47354ee6189707e05
Artem <yakovenko.art1@gmail.com>
2024-04-24 21:10:46 +0500

    main photo

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,5 +1,20 @@
+import photo from "../pictures/photo-central.png";
+
 export default function Main() {
-    return <section className="main">
-        
-    </section>;
+    return (
+        <section className="main">
+            <section className="main_central">
+                <section className="main_central_h1"></section>
+                <section className="main_central_composition">
+                    <div className="main_central_composition_circle-color"></div>
+                    <div className="main_central_composition_filter"></div>
+                    <div className="main_central_composition_circle-1"></div>
+                    <div className="main_central_composition_circle-2"></div>
+                    <div className="main_central_composition_circle-3"></div>
+                    <div className="main_central_composition_circle-2 circle-clone-rotate"></div>
+                    <img src={photo} alt="photo-central"></img>
+                </section>
+            </section>
+        </section>
+    );
 }
