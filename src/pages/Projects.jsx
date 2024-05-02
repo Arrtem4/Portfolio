65befc4a1ffae89e227139d9e91d6db2fa8e75a8
Artem <yakovenko.art1@gmail.com>
2024-05-02 16:31:10 +0500

    projects start

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,3 +1,16 @@
+import YearsIndex from "../components/YearsIndex";
 export default function Projects() {
-    return <section className="projects page-enter">Projects</section>;
+    return (
+        <section className="projects page-enter">
+            <section className="projects_year _2024">
+                <YearsIndex year={2024} />
+            </section>
+            <section className="projects_year _2023">
+                <YearsIndex year={2023} />
+            </section>
+            <section className="projects_year _2022">
+                <YearsIndex year={2022} />
+            </section>
+        </section>
+    );
 }
