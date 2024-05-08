cbd318254ead076970b1d30b3a57b4e3066977b9
Artem <yakovenko.art1@gmail.com>
2024-05-08 18:12:23 +0500

    cards start

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,15 @@
-export default function Card() {
+import { useState } from "react";
+
+export default function Card({ image }) {
+    const [rotate, setRotate] = useState(false);
     return (
-        <div className="card">
-            <div className="card-front"></div>
+        <div
+            className={`card ${rotate ? "rotate" : ""}`}
+            onTouchStart={() => setRotate(!rotate)}
+        >
+            <div className="card-front">
+                <img src={image} className="card-front_img"></img>
+            </div>
             <div className="card-back"></div>
         </div>
     );
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,8 +1,13 @@
 import Card from "../components/Card";
+import html from '../pictures/skills/html.webp'
 export default function Skills() {
     return (
         <section className="skills page-enter">
-            <Card />
+            <Card image={html} />
+            <Card image={html} />
+            <Card image={html} />
+            <Card image={html} />
+            <Card image={html} />
         </section>
     );
 }
