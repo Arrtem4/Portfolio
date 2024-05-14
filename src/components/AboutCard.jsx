0dba391217296d3cf680543063042852fb461a1c
Artem <yakovenko.art1@gmail.com>
2024-05-14 19:06:52 +0500

    cards order logic

--- src/components/AboutCard.jsx
+++ src/components/AboutCard.jsx
@@ -1,3 +1,14 @@
-export default function AboutCard({ text }) {
-    return <div className="about-card">{text}</div>;
+import { useState } from "react";
+
+export default function AboutCard({ text = "", indexIn = 0 }) {
+    const [index, setIndex] = useState(indexIn);
+    return (
+        <div
+            className="about-card center"
+            style={{ zIndex: index }}
+            onClick={() => setIndex(index - 5)}
+        >
+            {text}
+        </div>
+    );
 }
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,31 @@
 import { useTranslation } from "react-i18next";
 import AboutCard from "../components/AboutCard";
+// import { useState } from "react";
+
+// const cards = [
+//     {
+//         num: 1,
+//     },
+//     { num: 2 },
+//     { num: 3 },
+//     { num: 4 },
+//     { num: 5 },
+// ];
 export default function About() {
     const { t } = useTranslation();
+    // const [index1, setIndex1] = useState(999);
+    // const [index2, setIndex2] = useState(998);
+    // const [index3, setIndex3] = useState(997);
+    // const [index4, setIndex4] = useState(996);
+    // const [index5, setIndex5] = useState(995);
+
     return (
         <section className="about page-enter">
-            <AboutCard text={t("about.card1")} />
+            <AboutCard text={t("about.card1")} indexIn={999} />
+            <AboutCard text={t("about.card2")} indexIn={998} />
+            <AboutCard text={t("about.card3")} indexIn={997} />
+            <AboutCard text={t("about.card4")} indexIn={996} />
+            <AboutCard text={t("about.card5")} indexIn={995} />
         </section>
     );
 }
