8b9a5c2355ace861a3354fa3c53c2e7ef555f000
Artem <yakovenko.art1@gmail.com>
2024-05-16 00:15:15 +0500

    fix height

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -17,7 +17,7 @@ export default function About() {
                         key={card.num}
                         text={t(`about.card${card.num}`)}
                         h1={card.num === 1 ? t(`about.card1H1`) : ""}
-                        small = {card.num === 3 ? true : false}
+                        small={card.num === 3 ? true : false}
                         indexIn={9999 - card.num}
                         indexTotal={cards.length}
                         animationInProgress={animationInProgress}
