ccd7deacc1f5ee5b8bb45326673730156bc11c87
Artem <yakovenko.art1@gmail.com>
2024-05-15 16:42:30 +0500

    fix layout

--- src/pages/MainLayout.jsx
+++ src/pages/MainLayout.jsx
@@ -1,11 +1,10 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
-
 export default function MainLayout() {
     return (
-        <section className="main-layout">
+        <>
             <Header />
             <Outlet />
-        </section>
+        </>
     );
 }
